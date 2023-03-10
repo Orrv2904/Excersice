e1cebdf5c02c1b752436402873d52c6a1bc3baaa
Orrv2904 <Omarrv2904@gmail.com>
2023-03-10 11:26:59 -0600

    Creando desde SqlAlchemyORM

---
+++ models.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from models import *
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy()
+db.init_app(app)
+
+class Flight(db.Model):
+    __tablename__ = "flights2"
+    id = db.Column(db.Integer, primary_key=True)
+    origin = db.Column(db.String, nullable=False)
+    destination = db.Column(db.String, nullable=False)
+    duration = db.Column(db.Integer, nullable=False)
+
+
+class Passenger(db.Model):
+    __tablename__ = "passengers"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    flight_id = db.Column(db.Integer, db.ForeignKey("flights2.id"), nullable=False)
